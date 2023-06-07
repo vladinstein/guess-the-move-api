02e90d00bbc2ebad3087154bc2b860bbf0ca2ce9
Vladimir Goriachev <vladinstein@gmail.com>
2023-06-07 14:51:41 -0400

    uploading server configs

--- guess_the_move_api/__init__.py
+++ guess_the_move_api/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from stockfish import Stockfish
 from guess_the_move_api.config import Config
 
-stockfish = Stockfish('C:/Users/vladi/Downloads/stockfish_15.1_win_x64_popcnt/stockfish-windows-2022-x86-64-modern')
+stockfish = Stockfish('/home/vladinstein/stockfish_15.1_linux_x64/stockfish-ubuntu-20.04-x86-64')
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- guess_the_move_api/config.py
+++ guess_the_move_api/config.py
@@ -1,7 +1,10 @@
-import os
+import json
+
+with open('/etc/guess_the_move_config.json') as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('SECRET_KEY')
     SESSION_PERMANENT = True
     SESSION_TYPE = 'filesystem'
